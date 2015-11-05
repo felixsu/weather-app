e3d26e53278a372eed79e023b5ee54db713d665f
felix <felix.soewito@gmail.com>
2015-11-05 23:14:30 +0700

    get connectivity ready to use

---
+++ app/src/main/java/felix/com/weatherapp/AlertDialogFragment.java
@@ -0,0 +1,21 @@
+package felix.com.weatherapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by felix on 11/5/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.error_title)
+                .setMessage(R.string.error_message).setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/felix/com/weatherapp/MainActivity.java
+++ app/src/main/java/felix/com/weatherapp/MainActivity.java
@@ -1,5 +1,8 @@
 package felix.com.weatherapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -20,8 +24,8 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = MainActivity.class.getSimpleName();
-    double mLatitude = 0;
-    double mLongitude = 0;
+    double mLatitude = -6.215117;
+    double mLongitude = 106.824896;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +34,50 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().
-                url(buildUrl(mLatitude, mLongitude)).
-                build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "IO Exception");
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().
+                    url(buildUrl(mLatitude, mLongitude)).
+                    build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    alertUserAboutError();
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, response.body().string());
+                        } else {
+                            Log.v(TAG, response.body().string());
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception");
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "Network not available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialogFragment = new AlertDialogFragment();
+        dialogFragment.show(getFragmentManager(), "error_dialog");
     }
 
     private String buildUrl(double latitude, double longitude) {
