dc82f79273a63a4106c00116fa8c1a5cec46255e
felix <felix.soewito@gmail.com>
2015-11-06 19:50:33 +0700

    initial json working

--- app/src/main/java/felix/com/weatherapp/MainActivity.java
+++ app/src/main/java/felix/com/weatherapp/MainActivity.java
@@ -4,14 +4,9 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
@@ -20,12 +15,17 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = MainActivity.class.getSimpleName();
     double mLatitude = -6.215117;
     double mLongitude = 106.824896;
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,19 +49,38 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, response.body().string());
                         if (response.isSuccessful()) {
-                            Log.v(TAG, response.body().string());
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
-                            Log.v(TAG, response.body().string());
+                            alertUserAboutError();
                         }
-                    } catch (IOException e) {
-                        Log.e(TAG, "IO Exception");
+                    } catch (IOException | JSONException e) {
+                        Log.e(TAG, "Excepption caught", e);
                     }
                 }
             });
         } else {
             Toast.makeText(this, "Network not available", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timzeone = forecast.getString("timezone");
+
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timzeone);
+
+        Log.i(TAG, "From JSON " + currentWeather.getFormattedTime());
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable() {
